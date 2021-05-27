2837a4a4831556c1a9171d0f21b7db0ff11ff485
abdmahd <abdoumahdi19@outlook.fr>
2021-05-27 17:45:46 +0100

    change name 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Final ',
+      title: 'Final project',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
