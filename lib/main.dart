5c49fc36f85a353de01c1824ce3303fe292c9945
saber-2000 <84923570+saber-2000@users.noreply.github.com>
2021-05-27 17:48:34 +0100

    Update main.dart
    change to P

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Final project',
+      title: 'Final Project',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
